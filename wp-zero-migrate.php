6b4cd44dd098a4fa1c171bf84661df71dd783434
Lanigo <lanigouws@gmail.com>
2026-03-23 11:37:09 +0200

    Add main plugin file

---
+++ wp-zero-migrate.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WP Zero Migrate
+ * Plugin URI: https://github.com/Lanigo/wp-zero-migrate
+ * Description: A WordPress migration plugin with export/import support and no artificial size limits.
+ * Version: 0.1.0
+ * Author: Lanigo
+ * Author URI: https://github.com/Lanigo
+ */
+
+defined('ABSPATH') || exit;
